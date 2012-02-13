ffd873b1f5306b5a0e1b6731b4a41552ef8c8cda
Swizec Teller <swizec@swizec.com>
2012-02-13 13:00:31 +0100

    I'm an idiot

--- Learning/secrets_default.py
+++ Learning/secrets_default.py
@@ -1,3 +1,2 @@
-
-MIXPANEL = {'api_key': '421bc786342715b6a93e9190c91df3c1',
-            'api_secret': '5d71571f13046d2cb5a6422a70bbbf9d'}
+MIXPANEL = {'api_key': '',
+            'api_secret': ''}
