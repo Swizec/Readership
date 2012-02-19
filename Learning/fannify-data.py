2a9188c28d2404690c74e01d4330ed0bb9a66262
Swizec Teller <swizec@swizec.com>
2012-02-19 23:56:00 +0100

    creating a test and training data set

--- Learning/fannify-data.py
+++ Learning/fannify-data.py
@@ -4,6 +4,7 @@
 # The first line consists of three numbers: The first is the number of training pairs in the file, the second is the number of inputs and the third is the number of outputs.  The rest of the file is the actual training data, consisting of one line with inputs, one with outputs etc.
 
 from pymongo import Connection
+import random
 
 def extract(datapoint, keys):
     return reduce(list.__add__,
@@ -17,11 +18,25 @@ def inputs(datapoint):
 def outputs(datapoint):
     return extract(datapoint, ['readership'])
 
+def store(datapoints, filename):
+    f = open(filename, 'w')
+
+    f.write(str(len(datapoints))+" 14 "+" 2\n")
+    for datapoint in datapoints:
+        f.write(" ".join(map(str, inputs(datapoint)))+"\n")
+        f.write(" ".join(map(str, outputs(datapoint)))+"\n")
+
+    f.close()
+
 if __name__ == "__main__":
     db = Connection().readership_data
     readership = db.readership
 
-    print readership.count(), "14", "2"
-    for datapoint in readership.find():
-        print " ".join(map(str, inputs(datapoint)))
-        print " ".join(map(str, outputs(datapoint)))
+    datapoints = list(readership.find())
+    random.shuffle(datapoints)
+
+    # 70% training, 30% testing
+    store(datapoints[:int(len(datapoints)*0.7)],
+          "./training.dat")
+    store(datapoints[int(len(datapoints)*0.7):],
+          "./test.dat")
